483c214122650b55c3d17fddf8f76f90f9ba41ba
PiotrDabrowski232 <106407699+PiotrDabrowski232@users.noreply.github.com>
2022-07-18 22:02:00 +0200

    xd

--- MessAPI/Controllers/MessController.cs
+++ MessAPI/Controllers/MessController.cs
@@ -11,40 +11,18 @@ namespace MessAPI.Controllers
     [ApiController]
     public class MessController : ControllerBase
     {
-
-        public string[] MessTitles = new[]
-        {
-            "tytul1", "tytul2", "tytul3", "tytul4", "tytul5"
-        };
-        public string[] MessBody = new[]
-        {
-            "tresc1", "tresc2", "tresc3", "tresc4", "tresc5"
-        };
-       
-       /* private readonly ILogger<MessController> _logger;
+        private readonly MessService _logger;
 
         public MessController(ILogger<MessController> logger)
         {
-            _logger = logger;
-        }*/
+            _logger = new MessService();
+        }
 
         [HttpGet]
         public IEnumerable<MessaClass> Get()
         {
-            int indexerT = MessTitles.Length;
-            int indexerB = MessBody.Length;
-            return Enumerable.Range(1, 5).Select(index => new MessaClass
-            {
-                MessClassTitles = MessTitles[--indexerT],
-                MessClassBody = MessBody[--indexerB]
-            }).ToArray();
-            
+            return _logger.Get();
         }
        
-        [HttpPost]
-        public IActionResult Post(MessaClass payment)
-        {
-            
-        }
     }
 }
---
+++ MessAPI/MessService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessAPI
+{
+    public class MessService
+    {
+
+
+        /*  public List<string> MessTitles = new List<string>() { "tytul1", "tytul2", "tytul3", "tytul4", "tytul5" };
+
+          public List<string> MessBody = new List<string>() { "tresc1", "tresc2", "tresc3", "tresc4", "tresc5" };*/
+
+        public string[] MessTitles = new[]
+        {
+            "tytul1", "tytul2", "tytul3", "tytul4", "tytul5"
+        };
+        public string[] MessBody = new[]
+        {
+            "tresc1", "tresc2", "tresc3", "tresc4", "tresc5"
+        };
+
+        public IEnumerable<MessaClass> Get()
+        {
+            int indexerT = MessTitles.Length;
+            int indexerB = MessBody.Length;
+            return Enumerable.Range(1, 5).Select(index => new MessaClass
+            {
+                MessClassTitles = MessTitles[--indexerT],
+                MessClassBody = MessBody[--indexerB]
+            }).ToArray();
+        }
+    }
+}
