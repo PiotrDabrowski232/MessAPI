a510e601c9b533fd0b8d12fe79255cf1ef8d4e33
PiotrDabrowski232 <106407699+PiotrDabrowski232@users.noreply.github.com>
2022-08-07 11:58:01 +0200

    dziala

---
+++ MessAPI/Controllers/MessageControler.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+namespace MessAPI.Controllers
+{
+    
+    [Route("[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly IMessageRepository _service;
+        public MessageController(IMessageRepository service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<Message> Get()
+        {
+            return _service.ShowAllMessages();
+        }
+        
+        [HttpPost]
+        public ActionResult<IEnumerable<Message>> Post([FromBody] Message message)
+        {
+            return Ok(_service.AddMessage(message));
+        }
+        
+        [HttpPut]
+        public ActionResult<IEnumerable<Message>> Put2([FromBody] Message message)
+        {
+            return Ok(_service.ChangeMessage(message));
+        }
+
+        
+        [HttpDelete]
+        public ActionResult<IEnumerable<Message>> Delete([FromQuery] int id)
+        {
+            return Ok(_service.DeleteMessage(id));
+        }
+    }
+}
+
---
+++ MessAPI/Message.cs
@@ -0,0 +1,24 @@
+﻿namespace MessAPI
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+
+        public Message(int id)
+        {
+            Id = id;
+        }
+        public Message(int id, string title, string body)
+        {
+            Id = id;
+            Title = title;
+            Body = body;
+        }
+        public Message()
+        {
+        }
+
+    }
+}
